fc6298b0f28bc89cd584e0bf5ae1c487ec08d6fa
Marshall Belles <marshall.belles@milwaukeetool.com>
2021-09-14 22:41:39 -0400

    adjusting signing alg to see if it fixes a signing bug

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub mod flow {
 
 // for signing transactions
 use ring::rand;
-use ring::signature::{self, EcdsaKeyPair, ECDSA_P256_SHA256_FIXED_SIGNING};
+use ring::signature::{self, EcdsaKeyPair, ECDSA_P256_SHA256_ASN1_SIGNING};
 pub extern crate hex;
 use bytes::BytesMut;
 pub extern crate serde_rlp;
@@ -148,7 +148,7 @@ fn sign(
 ) -> Result<Vec<u8>, Box<dyn error::Error>> {
     let rng = rand::SystemRandom::new();
     let key_pair = EcdsaKeyPair::from_private_key_and_public_key(
-        &ECDSA_P256_SHA256_FIXED_SIGNING,
+        &ECDSA_P256_SHA256_ASN1_SIGNING,
         &hex::decode(private_key)?,
         &hex::decode(public_key)?,
     );
